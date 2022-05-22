9b2d1033e4cf05114136cbf8ad829fb01468c5cd
Lucas Korz <62682384+lucaskorz@users.noreply.github.com>
2022-05-22 13:09:39 -0300

    incluindo rotas de consulta a API publica swapi

--- src/routes/route-travel.ts
+++ src/routes/route-travel.ts
@@ -5,6 +5,7 @@ import { GetAllTravelController } from '../controllers/GetAllTravelController'
 import { GetTravelByIdController } from '../controllers/GetTravelByIdController'
 import { UpdateTravelController } from '../controllers/UpdateTravelController'
 import { checksTravel } from '../middleware/checksTravel'
+import fetch from 'node-fetch';
 
 const routerTravel = Router()
 
@@ -14,4 +15,37 @@ routerTravel.delete('/viagem/:id', DeleteTravelController)
 routerTravel.get('/viagem', GetAllTravelController)
 routerTravel.get('/viagem/:id', GetTravelByIdController)
 
-export default routerTravel
+const url: string = 'https://swapi.dev/api/'
+routerTravel.get('/personagens', async (req, res) => {
+  await fetch(url+'people')
+  .then((resp) => resp.json())
+  .then(function(data: any) {
+    return res.status(204).json(data.results)
+  })
+})
+
+routerTravel.get('/planetas', async (req, res) => {
+  await fetch(url+'planets')
+  .then((resp) => resp.json())
+  .then(function(data: any) {
+    return res.status(204).json(data.results)
+  })
+})
+
+routerTravel.get('/naves', async (req, res) => {
+  await fetch(url+'starships')
+  .then((resp) => resp.json())
+  .then(function(data: any) {
+  return res.status(204).json(data.results)
+  })
+})
+
+routerTravel.get('/especies', async (req, res) => {
+  await fetch(url+'species')
+  .then((resp) => resp.json())
+  .then(function(data: any) {
+    return res.status(204).json(data.results)
+  })
+})
+
+export default routerTravel;
